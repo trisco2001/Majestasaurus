05f377516951bd748a4d4f22c9cea471db74cc37
Tristan Leonard <tristanl@slalom.com>
2016-06-10 08:27:31 -0700

    Added the about the team page with sample text.

--- CarouselPageNavigation/TeamPage.xaml.cs
+++ CarouselPageNavigation/TeamPage.xaml.cs
@@ -13,6 +13,13 @@ namespace Majestasaurus.Portable
         public TeamPage()
         {
             InitializeComponent();
+
+            var teamPageModel = new
+            {
+                TeamDescription = "In general I know the weird al parody of a song much better than the original. I really like pop tarts but they're so bad for you.\n\nOh right, so I'm supposed to be talking about the team. Amanda has been studying art for thousands of years. This is my first Xamarin project, so bear with me. Xander has been testing video games for seven years. Aliera did not put the phone in her mouth."
+            };
+            this.BindingContext = teamPageModel;
+
         }
 
         public void BackClicked(object sender, EventArgs e)
